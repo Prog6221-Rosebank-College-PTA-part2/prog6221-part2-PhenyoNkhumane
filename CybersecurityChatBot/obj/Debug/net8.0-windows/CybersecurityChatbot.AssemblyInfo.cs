39bdf5073eb3f3ce3f6fcedbc0a23e1dd0d22ed2
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-05-29 19:52:07 +0200

    Update AssemblyInfo for versioning consistency and refresh cache files

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d923c85e145d2b25cdcdea5987176f6de71395f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177745cc3076924998c047bdab09287d06cbba55")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
