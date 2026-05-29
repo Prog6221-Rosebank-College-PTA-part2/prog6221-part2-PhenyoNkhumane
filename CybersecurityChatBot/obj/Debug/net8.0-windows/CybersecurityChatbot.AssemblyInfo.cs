23b7487673b91da2caf5b8e60b4a4f235eb6d221
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-05-29 21:40:57 +0200

    Remove unused App.xaml.cs and MainWindow.xaml.cs files

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c16de7e48788edd62cd596895a3d253b6a24eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc79057320f19cda87127b75b1ad66863d1c2e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
