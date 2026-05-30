770173a42ef31cc30a154a3f2a5dca7895e7ff19
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-05-30 19:47:46 +0200

    Commit

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fad2e4615e9d7df7ab97d4de074acf99d3aa9b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116a3145905a9160fc93341e01c1dbedd0095792")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
