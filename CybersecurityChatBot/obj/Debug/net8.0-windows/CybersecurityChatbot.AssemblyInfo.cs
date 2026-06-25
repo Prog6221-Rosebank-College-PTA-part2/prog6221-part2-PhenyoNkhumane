5f54371a04b7d9e0cb3fb21bc6dbf2c0a675e041
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-06-25 17:25:25 +0200

    Normalize file paths and update assembly informational version for consistency

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfeca41ccccf12d9f713f023b913da8dd66df852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5de65f4d946131f7133753ca21a72f5f19042f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
