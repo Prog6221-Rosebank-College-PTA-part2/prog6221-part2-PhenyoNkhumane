bfeca41ccccf12d9f713f023b913da8dd66df852
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-06-25 17:22:57 +0200

    Update assembly information and sourcelink for latest build version
    - Updated AssemblyInformationalVersion to reflect the new build identifier.
    - Modified sourcelink JSON to align with the updated assembly information.
    - Adjusted cache file to maintain project integrity.

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97df752580ba5a2484ead3cc43cf3cdab910b81c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfd7c47cbb587a2520944d8a7132d06dd97fd52")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
