97ce67349a22a6ff2ee8b446553ddaf87ff24bcd
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-06-26 09:55:35 +0200

    Update assembly version information and sourcelink references

--- CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatBot/obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e544a9b2d72b37b79c2dd2eb135ae3faea6260ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803db4ba11cd46df393a79b98a52da5d99a2f7de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
